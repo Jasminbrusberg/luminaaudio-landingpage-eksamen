91382ced0a0c1252f3be417b54ef8f8218b6dc40
Jasminbrusberg <J-brusberg@hotmail.com>
2026-05-20 19:47:14 +0200

    tilpasning af loop

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 "use strict";
 
 // laver loop sektion
+
 // const imgTrack=document.querySelector(".img-track"); finder listen med billeder
-const imgTrack = document.querySelector(".img-track");
+const imageTrack = document.querySelector(".img-track");
 // const originalImages = imgTrack.innerHTML Gemmer alt indholdet inde i listen. Altså alle dine <li> med billeder.
-const originalImages = imgTrack.innerHTML
+const originalImages = imageTrack.innerHTML;
 // imgTrack.innerHTML += originalImages; Kopierer billederne og sætter dem ind én gang mere. den hakker ikke
-imgTrack.innerHTML += originalImages;
+imageTrack.innerHTML += originalImages;
